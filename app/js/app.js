b89ff85791f3fe28e55c1b1fbc697c6a0ed92fec
gitMonks <thanksimran@gmail.com>
2017-02-25 12:43:19 +0000

    Wrote logic to select the current and next modal in app.js, by applying data-step and data-modal in index.html

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
-console.log('hello from inside app.js');
+const modals = document.querySelectorAll('.modal');
+const nextButtons = document.querySelectorAll('.modal .button')
+function showNext() {
+	const dataModal = this.dataset.modal;
+	const currentModal = document.querySelector(`[data-step="${dataModal}"]`);
+	const nextStep = parseInt(dataModal) + 1;
+	const nextModal = document.querySelector(`[data-step="${nextStep}"`)
+
+	currentModal.classList.remove('is-showing');
+	nextModal.classList.add('is-showing');
+	
+	console.log(currentModal);
+	console.log(nextModal);
+	console.log(nextStep, typeof nextStep);
+
+}
+
+
+nextButtons.forEach(button => button.addEventListener('click', showNext));
